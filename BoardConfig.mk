0beb99272c28475c8617133a31182fc5363e42ea
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    a72q: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := device/samsung/a72q
 include device/samsung/sm7125-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG        := vendor/pixelos-a72q_defconfig
+TARGET_KERNEL_CONFIG        := vendor/pixelos-a72q_defconfig vendor/debugfs.config
 BOARD_NAME                  := SRPTJ06B001
 
 # Camera

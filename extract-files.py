4dbabb78111b44cab2861b1a5232e8a5259d12a0
luk1337 <priv.luk@gmail.com>
2024-12-14 19:21:13 +0530

    a72q: Switch to py extract utils
    Change-Id: I10a2bc3472ff3294823a4565f80b558c6c14d9ec
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/sm7125-common',
+]
+
+module = ExtractUtilsModule(
+    'a72q',
+    'samsung',
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm7125-common', module.vendor
+    )
+    utils.run()

fbe5ad2c9130a8a18da897e41d56fa6a5c2a9ec3
koko-07870 <ko282ko82@gmail.com>
2025-01-06 19:11:49 +0530

    a72q: explicitly build updater
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- aosp_a72q.mk
+++ aosp_a72q.mk
@@ -31,6 +31,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
+IS_OFFICIAL := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_a72q

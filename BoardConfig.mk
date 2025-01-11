707d95deac30c4fa2d8d305ea6489088452f4eb6
koko-07870 <ko282ko82@gmail.com>
2025-01-11 12:25:16 +0530

    a72q: BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES = true
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,8 @@ DEVICE_PATH := device/samsung/a72q
 
 include device/samsung/sm7125-common/BoardConfigCommon.mk
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Kernel
 TARGET_KERNEL_CONFIG        := vendor/pixelos-a72q_defconfig vendor/debugfs.config
 BOARD_NAME                  := SRPTJ06B001

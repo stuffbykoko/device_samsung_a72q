6eca9307358c4c936e7963c5edf44ee1ddf8e6b9
koko-07870 <ko282ko82@gmail.com>
2025-02-13 14:30:06 +0530

    a72q: update to A725FXXUAFXL2
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- aosp_a72q.mk
+++ aosp_a72q.mk
@@ -41,8 +41,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a72qnsxx-user 11 RP1A.200720.012 A725FXXSAFXJ1 release-keys" \
-    BuildFingerprint=samsung/a72qnsxx/a72q:11/RP1A.200720.012/A725FXXSAFXJ1:user/release-keys
+    BuildDesc="a72qnsxx-user 11 RP1A.200720.012 A725FXXUAFXL2 release-keys" \
+    BuildFingerprint=samsung/a72qnsxx/a72q:11/RP1A.200720.012/A725FXXUAFXL2:user/release-keys
 
 # Security patch
 VENDOR_SECURITY_PATCH := 2024-11-01
